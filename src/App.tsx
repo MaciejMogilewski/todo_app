5ee34c262422721bc8e86cc0762dd12c7e27d802
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-04-29 13:59:40 +0200

    Styled add task

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
     Operation,
 } from './helpers/Api.ts';
 import AddSpentTimeForm from './components/AddSpentTimeForm';
+import { Button, Container, Stack, TextField } from '@mui/material';
 
 export interface TaskStatus {
     status: 'open' | 'closed';
@@ -120,27 +121,34 @@ function App() {
     }
 
     return (
-        <>
+        <Container maxWidth="md">
             <form
                 onSubmit={async e => {
                     e.preventDefault();
                     await handleSubmit();
                 }}
+                style={{ marginBottom: 40 }}
             >
-                <label htmlFor="task">Task</label>
-                <input
-                    type="text"
-                    id="task"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-                <label htmlFor="description">Description</label>
-                <textarea
-                    id="description"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                ></textarea>
-                <button type="submit">add</button>
+                <Stack spacing={2} direction="column">
+                    <TextField
+                        label="Title"
+                        variant="outlined"
+                        type="text"
+                        id="task"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <TextField
+                        label="Description"
+                        variant="outlined"
+                        id="description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <Button variant="contained" type="submit">
+                        Add
+                    </Button>
+                </Stack>
             </form>
             <div>
                 {tasks.map(task => (
@@ -211,7 +219,7 @@ function App() {
                     </div>
                 ))}
             </div>
-        </>
+        </Container>
     );
 }
 
